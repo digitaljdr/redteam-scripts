3603a883a6ccaefb208e9ab78493068611a8ec67
digitaljdr <digitaljdr@gmail.com>
2016-05-02 21:10:44 -0400

    Added files via upload

---
+++ runremotecmd.ps1
@@ -0,0 +1,1 @@
+﻿Enter-PSSession -ComputerName MU-T109-10 -Credential Student@MU-T109-10
---
+++ setservice.ps1
@@ -0,0 +1,2 @@
+﻿Get-Service -Name WinRM -ComputerName MU-T109-10 | Set-Service -Status Running
+Set-Item wsman:\localhost\client\trustedhosts *
